d97f069c2e54d3ce15623332b846d77da53c6967
Diego Pereira <dgo.andrade88@gmail.com>
2020-02-19 21:45:10 +0000

    Implemented crud and middleware

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 const db = mongoose.connect('mongodb://localhost:27017/mydb');
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 const app = express();
-const bookRouter = express.Router();
-const port = process.env.PORT || 4242;
+const port = process.env.PORT || 3000;
 
-bookRouter.route('/books')
-    .get((req, res) => {
-        const query = {};
-
-        if (req.query.title) {
-            query.title = req.query.title;
-        }
-
-        Book.find(query, (err, books) => {
-            if (err) {
-                res.send(err);
-            }
-
-            return res.json(books);
-        });
-    });
-
-    bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
-            if (err) {
-                res.send(err);
-            }
-
-            return res.json(book);
-        });
-    });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 app.get('/', (req, res) => {
